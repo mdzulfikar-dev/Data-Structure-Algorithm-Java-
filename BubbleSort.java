5304e94fe04fe5b11c70e5ecb9b5c3323b681da8
mdzulfikar-dev <zulfikarmd56@gmail.com>
2026-05-14 23:55:48 +0530

    Optimizing the BubbleSort algorithm by using a flag variable

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,21 +5,28 @@ public class BubbleSort {
     static void bubbleSort(int[] arr){
         int n = arr.length;
         for(int i =0;i<n-1;i++){
+            boolean  flag = false;
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    flag = true;
+                }
+                if(!flag){   // has nay swap happened
+                    return;
+
                 }
                 
             }
         }
     }
     public static void main(String[] args) {
-        int[]  arr = {23,76,1,46,6,78};
-        bubbleSort(arr);
+        // int[]  arr = {23,76,1,46,6,78};
+        int[]  arr1 = {1,2,3,4,5,6,7};
+        bubbleSort(arr1);
 
-        for(int elem:arr){
+        for(int elem:arr1){
             System.out.println(elem +" ");
         }
 
